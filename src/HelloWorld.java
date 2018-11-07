873e419ae5819cbea833418319c34581474063d3
benny-fields3rd <benny.fields3rd@gmail.com>
2018-11-07 11:04:26 -0600

    completed data types exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,42 @@
 public class HelloWorld {
     public static void main(String[] args) {
 //        System.out.println("Hello, World!");
-        System.out.print("Hello, ");
-        System.out.print("World!");
+//        System.out.print("Hello, ");
+//        System.out.print("World!");
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Codeup is awesome!";
+        System.out.println(myString);
+
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//          int x = 4;
+//          x += 5;
+//          System.out.println(x);
+
+//          int x = 4;
+//          int y = 4;
+//          y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(y);
     }
 }
