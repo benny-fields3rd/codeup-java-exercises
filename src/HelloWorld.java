0ef409d999729b3ce7618f52a80fe2b8c398849f
benny-fields3rd <benny.fields3rd@gmail.com>
2018-11-06 18:49:00 -0600

    first java push

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.print("Hello Again!!!");
+//        System.out.println("Hello, World!");
+        System.out.print("Hello, ");
+        System.out.print("World!");
     }
 }
